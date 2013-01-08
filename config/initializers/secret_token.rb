478600a43b578fdd6f02d0ff6947b259d03cbd9e
Nikhil Mungel <mail@hyfather.com>
2013-01-07 23:37:57 -0800

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spawner::Application.config.secret_token = '92cfafbf8d945dabd1e3783ba9e934a82cc1070da32659b8d75335af479e1459444206d1ccf42b15474b5afbe9cf5befcab15cf907c310c6fda257b4d733fd05'
