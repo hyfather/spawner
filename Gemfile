401959f28ae68df16dce0f528c4a3d77af44668c
Nikhil Mungel <mail@hyfather.com>
2013-01-10 01:28:43 -0800

    Installed devise and configured it for confirmable emails.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ gem 'rails', '3.2.8'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+gem 'devise'
+gem 'cancan'
+
 gem 'heroku-api'
 gem 'mixlib-shellout'
 gem 'git'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,10 @@ Spawner::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.delivery_method = :sendmail
+  
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
@@ -34,4 +36,6 @@ Spawner::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
